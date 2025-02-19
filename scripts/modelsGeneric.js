88024ba3916c0c6185a633031f1cc9a2a3f94844
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-19 15:42:25 -0300

    feat: table solicitacao pendente script

---
+++ scripts/modelsGeneric.js
@@ -0,0 +1,19 @@
+export const solicitacaoPendente = {
+    id_solicitacao: 0,
+    espaco_publico: {
+        id_espaco: 0,
+        nome: ""
+    },
+    solicitante: {
+        id_solicitante: 0,
+        nome: "",
+    },
+    tipo_evento: {
+        id_tipo_evento: 0,
+        descricao: null
+    },
+    periodo: {
+        data_inicio: "datetime",
+        data_fim: "datetime"
+    }
+};
---
+++ scripts/tbSolicitacaoPendente.js
@@ -0,0 +1,20 @@
+export function tbSolicitacaoPendente(data) {
+    const tableBody = document.getElementById("TbSolicitacaoPendente");
+
+    tableBody.innerHTML = "";
+
+    data.forEach(valor => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <tr>${valor.id_solicitacao}</tr>
+            <tr>${valor.espaco_publico.nome}</tr>
+            <tr>${valor.solicitante.nome}</tr>
+            <tr>${valor.tipo_evento.descricao}</tr>
+            <tr>${valor.periodo.data_inicio}</tr>
+            <tr>${valor.periodo.data_fim}</tr>
+            <tr>Aprovar</tr>
+            <tr>Negar</tr>
+        `;
+    });
+}
